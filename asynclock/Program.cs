db9b8223e18ec8f39713da48d0891e7ed9e54c9f
LeiYangGH <675686066@qq.com>
2020-09-15 12:46:18 +0800

    demo2

--- asynclock/Program.cs
+++ asynclock/Program.cs
@@ -10,21 +10,24 @@ namespace asynclock
     class Program
     {
         static int n = 0;
+        static object o = new object();
+
+        //这里是模拟一些库函数，也就是不属于当前项目，无法修改
         static void WriteIncreasedNumber()
         {
-            Console.WriteLine(n++);
-            Thread.Sleep(1000);//模拟一些耗时操作
+            Task.Run(() =>
+            {
+                Console.WriteLine(n++);
+                Thread.Sleep(100);//模拟一些耗时操作
+            });
+
         }
         //目标：让输出的数字变得有序递增
         static void Main(string[] args)
         {
             for (int i = 0; i < 10; i++)
             {
-                //不要去掉Task.Run，是为了异步执行，不阻塞主线程
-                Task.Run(() =>
-                {
-                    WriteIncreasedNumber();
-                });
+                WriteIncreasedNumber();
             }
             Console.ReadKey();
         }
